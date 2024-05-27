7d2377d2b359a6cf5ddd8080074e1b4f42c7ff1c
QuaterX <sztanderamras@gmail.com>
2024-05-27 11:37:03 +0200

    2

--- phpv2.py
+++ phpv2.py
@@ -3,90 +3,137 @@ import re
 import mysql.connector
 from appJar import gui
 
+app = gui("PhPmyAdminV2", "1100x800")
+
 dblist = list()
 tblist = list()
 
 
 def databases():
-    x = mydblogin.cursor()
-    x.execute("SHOW DATABASES")
-    for y in x:
-        dblist.append(y)
+    query = mydblogin.cursor()
+    query.execute("SHOW DATABASES")
+    for database_instances in query:
+        dblist.append(database_instances)
 
 
 def tables():
-    e = 0
-    for h in tblist:
-        e += 1
+    tblist_length = len(tblist)
     tblist.clear()
 
-    z = app.getOptionBox("Databases")
-    db = re.sub("[(,)]", "", z)
-    print(db)
-    x = mydblogin.cursor()
-    sql = "SELECT table_name FROM information_schema.tables WHERE table_schema = " + db
-    print(sql)
-    x.execute(sql)
-    for y in x:
-        tblist.append(y)
-    if e > 0:
+    database = app.getOptionBox("Databases")
+    dbname = re.sub("[(,)]", "", database)
+
+    query = mydblogin.cursor()
+    sql = "SELECT table_name FROM information_schema.tables WHERE table_schema = " + dbname
+
+    query.execute(sql)
+    for instances in query:
+        tblist.append(instances)
+    if tblist_length > 0:
         app.changeOptionBox("Tables", tblist)
 
 
-def databaselogin(h, u, p):
-    try:
-        global mydblogin
-        mydblogin = mysql.connector.connect(
-            host=h,
-            user=u,
-            password=p
-        )
-        app.hideSubWindow("Login")
-        app.show()
+class GuiClass:
+
+    @staticmethod
+    def initialize_main():
+        app.setBg("orange")
+        app.setFont(18)
+
         databases()
+
         app.addOptionBox("Databases", dblist)
         app.setOptionBoxChangeFunction("Databases", tables)
+
         tables()
+
         app.addOptionBox("Tables", tblist)
-    except:
-        app.setLabel("title", "Login failure")
+
+        app.hideSubWindow("Login")
+
+        gui.sql_subwindow()
+
+        app.addButton("SQL", gui.show_sql_subwindow)
+
+        app.show()
+
+    @staticmethod
+    def login_subwindow():
+        app.startSubWindow("Login")
+
+        app.addLabel("login_title", "Please login")
+        app.addLabelEntry("IP")
+        app.addLabelEntry("Username")
+        app.addSecretLabelEntry("Password")
+        app.setFocus("Username")
+        app.addNamedButton("Submit", "login_submit", button.login_submit)
+        app.addNamedButton("Cancel", "login_cancel", button.cancel)
+
+        app.stopSubWindow()
+
+    @staticmethod
+    def sql_subwindow():
+        app.startSubWindow("SQLQuery")
+
+        app.addLabel("sql_title", "SQL query executor")
+        app.addScrolledTextArea("SQL")
+        app.addNamedButton("Submit", "sql_submit" , button.sql_submit)
+
+        app.stopSubWindow()
+
+    @staticmethod
+    def sql_faliure():
+        app.setLabel("sql_title", "Something went wrong with your syntax")
+
+    @staticmethod
+    def sql_succes(query):
+        app.setLabel("sql_title", query)
+
+    @staticmethod
+    def show_sql_subwindow():
+        app.showSubWindow("SQLQuery")
 
 
-def press(button):
-    if button == "Cancel":
+class ButtonPress:
+    @staticmethod
+    def cancel():
         app.stop()
-    else:
+
+    @staticmethod
+    def login_submit():
         ip = app.getEntry("IP")
         user = app.getEntry("Username")
         if len(app.getEntry("Password")) > 0:
-            password = app.getEntry("Password")
+            passw = app.getEntry("Password")
         else:
-            password = ""
-        databaselogin(ip, user, password)
+            passw = ""
+        databaselogin(ip, user, passw)
+
+    @staticmethod
+    def sql_submit():
+        sql = app.getTextArea("SQL")
+        if len(sql) > 0:
+            try:
+                query = mydblogin.cursor()
+                query.execute(sql)
+                gui.sql_succes(query)
+            except:
+                gui.sql_faliure()
 
 
-app = gui("PhPmyAdminV2", "1100x800")
-app.setBg("orange")
-app.setFont(18)
+def databaselogin(hostname, username, password):
+    global mydblogin
+    mydblogin = mysql.connector.connect(
+        host=hostname,
+        user=username,
+        password=password
+    )
+    gui.initialize_main()
 
-# Login subwindow beginning
-app.startSubWindow("Login")
 
-app.addLabel("title", "Please login")
-app.addLabelEntry("IP")
-app.addLabelEntry("Username")
-app.addSecretLabelEntry("Password")
-app.setFocus("Username")
-app.addButtons(["Submit", "Cancel"], press)
+button = ButtonPress
 
-app.stopSubWindow()
-# Login subwindow ending
-
-# SQL Query subwindow beginning
-app.startSubWindow("SQL Query")
-
-app.addLabel("title2", "Please write query in the textbox")
-
-app.stopSubWindow()
+gui = GuiClass
+gui.login_subwindow()
 
 app.go(startWindow="Login")
